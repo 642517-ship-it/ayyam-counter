7e61006419671d26ede4471957ace5d35cd69bcf
khalil <642517@gmail.com>
2026-09-04 22:48:26 +0300

    bump cache to v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ayyam-v1';
+const CACHE = 'ayyam-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
